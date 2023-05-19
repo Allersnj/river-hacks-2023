14424a4c8b113c1975efe4e22450cc99ea3f2a6a
Nathan Allers <nateallers@gmail.com>
2023-05-19 14:08:23 -0500

    Add resources to ResourceModal

--- moodtracker/src/components/ResourceModal.jsx
+++ moodtracker/src/components/ResourceModal.jsx
@@ -1,26 +1,35 @@
 import react from 'react'
 import './Modal.css'
 
-function ResourceModal(props)
-{
-    return (
-        <div className='modalBackground'>
-            <div className='modalContainer'>
-                <div className="title">
-                    <h1>Resources</h1>
-                </div>
-                <div className="body">
-                  <p>It seems you've been feeling a little down lately. Here are some online resources that may be able to help.</p>
-
-                </div>
-                <div className="footer">
-                    <button onClick={() => props.closeModal(false)}>Close</button>
-                </div>
-
-            </div>
+function ResourceModal(props) {
+  return (
+    <div className='modalBackground'>
+      <div className='modalContainer'>
+        <div className="title">
+          <h1>Resources</h1>
         </div>
-
-    )
+        <div className="body">
+          <p>It seems you've been feeling down lately. Here are some online resources that may be able to help.</p>
+        </div>
+        <div className="body">
+            <ul id='resources'>
+              <li>
+                <a href='https://mhanational.org/im-looking-mental-health-help-myself'>Mental Health America</a>
+              </li>
+              <li>
+                <a href='https://www.thenationalcouncil.org/get-involved/members/'>Mental Health Community Centers</a>
+              </li>
+              <li>
+                <a href='https://www.nimh.nih.gov/health'>National Institute of Mental Health</a>
+              </li>
+            </ul>
+          </div>
+        <div className="footer">
+          <button onClick={() => props.closeModal(false)}>Close</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ResourceModal
