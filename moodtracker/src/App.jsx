d81556c926e25f3529b8e3d86cf1b7f4ec5ec90e
Nathan Allers <nateallers@gmail.com>
2023-05-19 11:09:05 -0500

    Change Modal to import Entry

--- moodtracker/src/App.jsx
+++ moodtracker/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Entry from './components/Entry.jsx'
 import Post from './components/Post.jsx'
 import Modal from './components/Modal.jsx'
 
